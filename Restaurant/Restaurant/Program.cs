4205b81b81344e9c2e5784eb983aa2204126fd6e
altair961 <altair89@list.ru>
2024-08-07 08:33:14 +0300

    Returning string was changed to check CI CD docker correctness.

--- Restaurant/Restaurant/Program.cs
+++ Restaurant/Restaurant/Program.cs
@@ -7,7 +7,7 @@ namespace Restaurant
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Hello World! Again!");
 
             app.Run();
         }
